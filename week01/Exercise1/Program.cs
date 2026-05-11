615a082f4be4e6226d191b9c18118fb4e2e646cb
owms-droid <olivermodesto87@gmail.com>
2026-05-11 12:17:17 -0500

    exercises week 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine("Please enter your first name:");
+        string fname = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name:");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("Please enter your grade (0-100):");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Better luck next time!");
+        }
     }
 }
