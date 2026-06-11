05b404aed41232b8dc6f74a79b4d47724a749671
owms-droid <olivermodesto87@gmail.com>
2026-06-11 12:11:50 -0500

    Learning activity week 06 is completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Purple", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Red", 4, 6);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
